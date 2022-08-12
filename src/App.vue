7de4c3de7a4beac222961c1ed985ec7cf9387cf6
Vladimir Kartyshov <Kratos@beelinerouter.net>
2022-08-12 19:07:07 +0300

    initional project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Список задач</h1>
+    <div>Осталось сделать задач: <span class="counter">2</span></div>
+    <div class="list">
+      <div class="item done">
+        <input type="checkbox" checked />
+        Развернуть окружение в Codepen
+      </div>
+      <div class="item">
+        <input type="checkbox" />
+        Пройти курс по Vue
+      </div>
+      <div class="item">
+        <input type="checkbox" />
+        Сделать интернет-магазин на Vue
+      </div>
+    </div>
+    <div class="form">
+      <input />
+      <button type="button">Добавить</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: powderblue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+}
+.list {
+  padding: 20px;
+  border: 1px solid #ccc;
+  margin: 20px 0;
+  width: 300px;
+}
+.item {
+  margin: 10px 0;
+}
+.done {
+  text-decoration: line-through;
+}
+.form {
+  margin: 20px 0;
 }
 </style>
