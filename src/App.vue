a47eba8d115f83b990f10ac86d4c35fbda53c850
Vladimir Kartyshov <Kratos@beelinerouter.net>
2022-08-19 15:11:31 +0300

    block 2

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <template>
   <div id="app">
-    <h1>Список задач</h1>
-    <div>Осталось сделать задач: <span class="counter">2</span></div>
+    <h1 @mouseover="onHover">Список задач</h1>
+    <div v-if="count() == 0">Job well done</div>
+    <div v-else-if="count() == 1">Осталась всего одна задача</div>
+    <div v-else>
+      Осталось сделать задач: <span class="counter">{{ count() }}</span>
+    </div>
     <div class="list">
-      <div class="item" v-for="(task, index) in tasks" :key="index">
-        <input type="checkbox" checked />
-        {{ task }}
+      <div
+        class="item"
+        :class="{done: task.done}"
+        v-for="(task, index) in tasks"
+        :key="index"
+      >
+        <input type="checkbox" v-model="task.done" />
+        {{ task.text }}
       </div>
-      <div class="item-done" v-for="(task, index) in tasks2" :key="index">
-        <input type="checkbox" checked />
-        {{ task }}
-      </div>
+      <!-- <div
+        class="item"
+        :class="{done: task.done}"
+        v-for="(task, index) in tasks2"
+        :key="index"
+      >
+        <input type="checkbox" v-model="task.done" />
+        {{ task.text }}
+      </div> -->
     </div>
     <div class="form">
-      <input :placeholder="message" />
-      <button type="button" :disabled="isSubmitting">Добавить</button>
+      <input @mouseover="onHover" v-model="message" @keydown.enter="addTask" />
+      <button type="button" @click="addTask" :disabled="isSubmitting">
+        Добавить
+      </button>
     </div>
+    <transition name="anm">
+      <img
+        class="image"
+        src="https://images.unsplash.com/photo-1509479200622-4503f27f12ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"
+        v-show="count() == 0"
+      />
+    </transition>
   </div>
 </template>
 
@@ -25,14 +48,30 @@ export default {
   data() {
     return {
       message: 'Hello vue',
-      isSubmitting: true,
+      isSubmitting: false,
       tasks: [
-        'Развернуть окружение в Codepen',
-        'Пройти курс по Vue',
-        'Сделать интернет-магазин на Vue',
+        {text: 'Развернуть окружение в Codepen', done: true},
+        {text: 'Пройти курс по Vue', done: false},
+        {text: 'Сделать интернет-магазин на Vue', done: false},
       ],
-      tasks2: ['Начал курс vue skillbox', 'Купил пива', 'Покормил кота'],
+      // tasks2: [
+      //   {text: 'Начал курс vue skillbox', done: false},
+      //   {text: 'Купил пива и креветки', done: true},
+      //   {text: 'Покормил кота', done: true},
+      // ],
     }
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({text: this.message, done: false})
+      this.message = ''
+    },
+    count() {
+      return this.tasks.filter((task) => !task.done).length
+    },
+    onHover: function (e) {
+      e.target.style.color = 'red'
+    },
   },
 }
 </script>
@@ -54,11 +93,40 @@ export default {
 .item {
   margin: 10px 0;
 }
-.item-done {
+.done {
   text-decoration: line-through;
-  padding-top: 10px;
 }
 .form {
   margin: 20px 0;
+}
+.image {
+  width: 500px;
+}
+.anm-enter {
+  opacity: 0;
+}
+.anm-enter-active {
+  transition: opacity 8s;
+}
+/* .anm-enter-to {
+  animation: 3s anm-slide;
+} */
+/* .anm-leave {
+  animation: 3s anm-slide backwards;
+} */
+.anm-leave-active {
+  animation: 3s anm-slide forwards;
+  transition: opacity 1s;
+}
+.anm-leave-to {
+  opacity: 0;
+}
+@keyframes anm-slide {
+  from {
+    transform: translateX(0px);
+  }
+  to {
+    transform: translateX(-100px);
+  }
 }
 </style>
