e7a485ac6d482a0ff01d6a0aee6ebfbfe886822f
Vladimir Kartyshov <Kratos@beelinerouter.net>
2022-08-22 04:57:05 +0300

    block 3 task 1

--- src/components/LikeButton.vue
+++ src/components/LikeButton.vue
@@ -1,4 +1,4 @@
-<template> // исправлен коммит в git , для удобства проверки
+<template> 
   <div class="container">
     <div>
       <button class="ml" @mouseover="onHover" type="button" @click="counter++">
